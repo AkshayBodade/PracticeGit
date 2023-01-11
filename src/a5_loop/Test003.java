b7e1931d9fc4570b133a0b3819511cb1bb34dd88
AkshayBodade <akshaybodade37@gmail.com>
2023-01-11 18:21:34 +0530

    Test003 add

---
+++ src/a5_loop/Test003.java
@@ -0,0 +1,19 @@
+package a5_loop;
+
+public class Test003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int r,n=4,c;
+		for(r=0;r<n;r++)
+		{
+			for(c=n;c>r;c--)
+				System.out.print("*");
+			System.out.print("\n");
+		
+
+	}
+
+}
+}
